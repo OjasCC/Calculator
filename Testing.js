cd3939b6fa2bbda852d6d16bea856223aa175167
Ojas Gupta <ojasgupta206@gmail.com>
2023-11-12 12:00:00 +0000

    Testing

--- Testing.js
+++ Testing.js
@@ -10,18 +10,21 @@ let text=data.toString()
 // .commit("Testing", { "--date":"2023-11-28"})
 // .push('origin', 'main');
 
-for (let line of text.split("\n")) {
-    line=line.split(" ")[0].replace("GIT_AUTHOR_DATE=","")
-    console.log(line)
+async function testing(){
+    for (let line of text.split("\n")) {
+        line=line.split(" ")[0].replace("GIT_AUTHOR_DATE=","")
+        console.log(line)
     
-    fs.writeFileSync('./Dummy.txt', line);
-
-    simpleGit()
-    .add(["./*"])
-    .commit("Testing", { "--date":line})
-    .push('origin', 'main');
+        fs.writeFileSync('./Dummy.txt', line);
+    
+        await simpleGit()
+        .add(["./*"])
+        .commit("Testing", { "--date":line})
+        .push('origin', 'main');
+    }
 }
 
+testing()
 // console.log(text)
 
 // simpleGit().add(["./Testing.js"]).commit("Testing", { "--date":"2023-12-06"})
