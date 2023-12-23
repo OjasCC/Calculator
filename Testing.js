00a60a5510628c2067bbf83f92cf57cb09bfd8d3
Ojas Gupta <ojasgupta206@gmail.com>
2023-12-23 12:00:00 +0000

    Testing

--- Testing.js
+++ Testing.js
@@ -1,25 +1,22 @@
 const simpleGit = require('simple-git');
 var fs = require('fs');
 
-
-// simpleGit().add(["./Testing.js"]).commit("Testing", { "--date":"2023-12-06"})
-
 var data = fs.readFileSync('data.txt', 'utf8');
 let text=data.toString()
 
 
-simpleGit()
-.add(["./*"])
-.commit("Testing", { "--date":"2023-11-28"})
-.push('origin', 'main');
+// simpleGit()
+// .add(["./*"])
+// .commit("Testing", { "--date":"2023-11-28"})
+// .push('origin', 'main');
 
-// for (let line of text.split("\n")) {
-//     line=line.split(" ")[0].replace("GIT_AUTHOR_DATE=","")
-//     simpleGit()
-//     .add(["./*"])
-//     .commit("Testing", { "--date":line, "--allow-empty":""})
-//     .push('origin', 'main');
-// }
+for (let line of text.split("\n")) {
+    line=line.split(" ")[0].replace("GIT_AUTHOR_DATE=","")
+    simpleGit()
+    .add(["./*"])
+    .commit("Testing", { "--date":line})
+    .push('origin', 'main');
+}
 
 // console.log(text)
 
